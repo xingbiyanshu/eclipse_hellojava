7b419dd2aca894ebc5aa8faed8595cebf2082e81
xingbiyanshu <xingbiyanshu@gmail.com>
2019-09-01 10:03:53 +0800

    first commit

---
+++ src/com/sissi/java/study/HelloJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sissi.java.study;
+
+/**
+ * @author DELL
+ *
+ */
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java!");
+	}
+
+}
